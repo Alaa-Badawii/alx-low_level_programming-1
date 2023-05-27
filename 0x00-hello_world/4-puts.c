116ccfcaf3de32b71adacce78faccb5cf8fb57cb
root <root@DESKTOP-01PIF4D>
2023-05-27 10:13:04 +0300

    finally

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - a main function of the program that print a sentence
- * Return: 0 (success)
- */
+ *  * main - a main function of the program that print a sentence
+ *   * Return: 0 (success)
+ *    */
 int main(void)
 {
-puts("\"Programminh is like building a multilingual puzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
